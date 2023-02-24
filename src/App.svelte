42f79648670b8dd62f4e63208bd3c5e1a2fc6129
LirikTop <melnicukkiril2002@gmail.com>
2023-02-24 20:38:00 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Description from "./Description.svelte";
+	import Title from "./Title.svelte";
+</script>
+
+<main>
+	<Title />
+	<Description />
+</main>
---
+++ src/Description.svelte
@@ -0,0 +1,18 @@
+<script>
+    let count = 0;
+    $: doubleCount = count * 2 ;
+
+    $: switch (count) {
+            case 11:
+                alert("count is dangerously high!");
+                count = 0;
+                break;
+        }
+
+    function clickButton() {
+        count++;
+    }
+</script>
+
+<p>Count: {count}, double: {doubleCount}</p>
+<button on:click={clickButton}> Click </button>
