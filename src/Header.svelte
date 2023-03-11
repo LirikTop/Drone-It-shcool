81ad86fc34fd219bef3adced2de80a23f61f1059
LirikTop <melnicukkiril2002@gmail.com>
2023-03-12 01:08:30 +0200

    add animation 12.03.2023

--- src/Header.svelte
+++ src/Header.svelte
@@ -40,10 +40,10 @@
 
     <div class="container-row">
         <div class="left">
-            <img src={img_drone} alt="Drone"/>
+            <img src={img_drone} alt="Drone" data-aos="fade-right" data-aos-duration="1000"/>
         </div>
 
-        <div class="right"data-aos="fade-up" data-aos-duration="1000">
+        <div class="right" data-aos="fade-up" data-aos-duration="1000">
             <h1>Drone?</h1>
             <h1>You Need a REFLEX 3</h1>
             <p>
